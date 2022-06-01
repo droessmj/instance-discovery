950d0efb5dd80d08d2bf991c2fbe0ade8800921c
Michael Droessler <michael.droessler@lacework.net>
2022-05-31 20:59:46 -0500

    arguments

--- instances_without_agents.py
+++ instances_without_agents.py
@@ -1,6 +1,23 @@
 from datetime import datetime, timedelta, timezone
 from laceworksdk import LaceworkClient
 import json
+import argparse
+import logging
+import os
+
+MAX_RESULT_SET = 500_000
+LOOKBACK_DAYS = 1
+
+
+class InstanceResult():
+    def __init__(self, instances_without_agents, instances_with_agents, agents_without_inventory) -> None:
+        self.instances_without_agents = list(instances_without_agents)
+        self.instances_with_agents = list(instances_with_agents)
+        self.agents_without_inventory = list(agents_without_inventory)
+
+    def toJson(self):
+        return json.dumps(self.__dict__, indent=4, sort_keys=True)
+
 
 def check_truncation(results):
     if results is list:
@@ -8,16 +25,13 @@ def check_truncation(results):
             return True
     return False
 
-# WHAT METADATA DO WE HAVE AVAILABLE TO CONSISTENTLY CHECK UNIQUENESS ACROSS GCP & AWS? 
-# AWS - ARN
-# GCP - ???
+
 def normalize_input(input, identifier):
     normalized_output = list()
     if len(input) > 0:
         data = input[0]['data']
         for r in data:
             if identifier == 'agent':
-                # print(r)
                 if 'VmProvider' in r['tags'].keys() and r['tags']['VmProvider'] == 'GCE':
                     normalized_output.append(r['hostname'])
                 elif 'VmProvider' in r['tags'].keys() and r['tags']['VmProvider'] == 'AWS':
@@ -36,76 +50,157 @@ def normalize_input(input, identifier):
     return normalized_output
 
 
-MAX_RESULT_SET = 500_000
-LOOKBACK_DAYS = 1
+def main(args):
 
-# lookback time can be configured for any value between 1 hour and 7 days
-current_time = datetime.now(timezone.utc)
-start_time = current_time - timedelta(days=LOOKBACK_DAYS)
-start_time = start_time.strftime('%Y-%m-%dT%H:%M:%SZ')
-end_time = current_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+    try:
+        client = LaceworkClient(
+            account=args.account,
+            subaccount=args.subaccount,
+            api_key=args.api_key,
+            api_secret=args.api_secret,
+            profile=args.profile
+        )
+    except Exception:
+        raise
 
-client = LaceworkClient(profile='lwcs')
+    if args.debug:
+        logger.setLevel('DEBUG')
+        logging.basicConfig(level=logging.DEBUG)
 
-all_agent_instances = client.agent_info.search(json={
-        'timeFilter': { 
-            'startTime' : start_time, 
-            'endTime'   : end_time
-        } 
-    })
-list_agent_instances = normalize_input(list(all_agent_instances), 'agent')
-if check_truncation(list_agent_instances):
-    print(f'WARNING: Agent Instances truncated at {MAX_RESULT_SET} records')
+    current_time = datetime.now(timezone.utc)
+    start_time = current_time - timedelta(days=LOOKBACK_DAYS)
+    start_time = start_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+    end_time = current_time.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-print(f'Agent Instances: {list_agent_instances}\n')
-
-
-gcp_inventory = client.inventory.search(json={
-        'timeFilter': { 
-            'startTime' : start_time, 
-            'endTime'   : end_time
-        }, 
-        'filters': [
-            # pubsub.googleapis.com/Topic
-            { 'field': 'resourceType', 'expression': 'eq', 'value':'compute.googleapis.com/Instance'}
-        ],
-        'dataset': 'GcpCompliance'
-    })
-list_gcp_instances = normalize_input(list(gcp_inventory), 'Gcp')
-if check_truncation(list_gcp_instances):
-    print(f'WARNING: GCP Instances truncated at {MAX_RESULT_SET} records')
-print(f'GCP Instances: {list_gcp_instances}\n')
+    all_agent_instances = client.agent_info.search(json={
+            'timeFilter': { 
+                'startTime' : start_time, 
+                'endTime'   : end_time
+            } 
+        })
+    list_agent_instances = normalize_input(list(all_agent_instances), 'agent')
+    if check_truncation(list_agent_instances):
+        logger.warning(f'WARNING: Agent Instances truncated at {MAX_RESULT_SET} records')
+    logger.debug(f'Agent Instances: {list_agent_instances}\n')
 
 
-aws_inventory = client.inventory.search(json={
-        'timeFilter': { 
-            'startTime' : start_time, 
-            'endTime'   : end_time
-        }, 
-        'filters': [
-            { 'field': 'resourceType', 'expression': 'eq', 'value':'ec2:instance'}
-        ],
-        'dataset': 'AwsCompliance'
-    })
-list_aws_instances = normalize_input(list(aws_inventory), 'Aws')
-if check_truncation(list_aws_instances):
-    print(f'WARNING: AWS Instances truncated at {MAX_RESULT_SET} records')
-print(f'AWS Instances: {list_aws_instances}\n')
+    gcp_inventory = client.inventory.search(json={
+            'timeFilter': { 
+                'startTime' : start_time, 
+                'endTime'   : end_time
+            }, 
+            'filters': [
+                { 'field': 'resourceType', 'expression': 'eq', 'value':'compute.googleapis.com/Instance'}
+            ],
+            'dataset': 'GcpCompliance'
+        })
+    list_gcp_instances = normalize_input(list(gcp_inventory), 'Gcp')
+    if check_truncation(list_gcp_instances):
+        logger.warning(f'WARNING: GCP Instances truncated at {MAX_RESULT_SET} records')
+    logger.debug(f'GCP Instances: {list_gcp_instances}\n')
 
-# SET WORK
-all_instances = set(list_aws_instances) | set(list_gcp_instances)
 
-# difference, not symmetric difference (^)
-instances_without_agents = all_instances - set(list_agent_instances)
+    aws_inventory = client.inventory.search(json={
+            'timeFilter': { 
+                'startTime' : start_time, 
+                'endTime'   : end_time
+            }, 
+            'filters': [
+                { 'field': 'resourceType', 'expression': 'eq', 'value':'ec2:instance'}
+            ],
+            'dataset': 'AwsCompliance'
+        })
+    list_aws_instances = normalize_input(list(aws_inventory), 'Aws')
+    if check_truncation(list_aws_instances):
+        logger.warning(f'WARNING: AWS Instances truncated at {MAX_RESULT_SET} records')
+    logger.debug(f'AWS Instances: {list_aws_instances}\n')
 
-if len(instances_without_agents) > 0:
-    print (f'Instances without agent:')
-    for instance in instances_without_agents:
-        print(f'\t{instance}')
+    all_instances_inventory = set(list_aws_instances) | set(list_gcp_instances)
 
-print('\n')
+    instances_without_agents = list()
+    matched_instances = list()
+    for instance_urn in all_instances_inventory:
+        if all(agent_instance not in instance_urn for agent_instance in list_agent_instances):
+            instances_without_agents.append(instance_urn)
+        else:
+            matched_instances.append(instance_urn)
 
-if len(list_agent_instances) > 0:
-    print(f'Instances with agent:')
+    agents_without_inventory = list()
     for instance in list_agent_instances:
-        print(f'\t{instance}')
+        if not any(instance in instance_urn for instance_urn in matched_instances):
+            agents_without_inventory.append(instance)
+
+    if args.json:
+        instance_result = InstanceResult(instances_without_agents, matched_instances, agents_without_inventory)
+        print(instance_result.toJson())
+    else:
+        if len(instances_without_agents) > 0:
+            print(f'Instances without agent:')
+            for instance in instances_without_agents:
+                print(f'\t{instance}')
+            print('\n')
+
+        if len(matched_instances) > 0:
+            print(f'Instances reconciled with agent:')
+            for instance in matched_instances:
+                print(f'\t{instance}')
+            print('\n')
+
+        if len(agents_without_inventory) > 0:
+            print(f'Agents without corresponding inventory:')
+            for instance in agents_without_inventory:
+                print(f'\t{instance}')
+            print('\n')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='A script to automatically issue container vulnerability scans to Lacework based on running containers'
+    )
+    parser.add_argument(
+        '--account',
+        default=os.environ.get('LW_ACCOUNT', None),
+        help='The Lacework account to use'
+    )
+    parser.add_argument(
+        '--subaccount',
+        default=os.environ.get('LW_SUBACCOUNT', None),
+        help='The Lacework sub-account to use'
+    )
+    parser.add_argument(
+        '--api-key',
+        dest='api_key',
+        default=os.environ.get('LW_API_KEY', None),
+        help='The Lacework API key to use'
+    )
+    parser.add_argument(
+        '--api-secret',
+        dest='api_secret',
+        default=os.environ.get('LW_API_SECRET', None),
+        help='The Lacework API secret to use'
+    )
+    parser.add_argument(
+        '-p', '--profile',
+        default=os.environ.get('LW_PROFILE', None),
+        help='The Lacework CLI profile to use'
+    )
+    parser.add_argument(
+        '--json',
+        default=False,
+        action='store_true',
+        help='Emit results as json for machine processing'
+    )
+    parser.add_argument(
+        '--debug',
+        action='store_true',
+        default=os.environ.get('LW_DEBUG', False),
+        help='Enable debug logging'
+    )
+    args = parser.parse_args()
+
+    logging.basicConfig(
+        format='%(asctime)s %(name)s [%(levelname)s] %(message)s'
+    )
+    logger = logging.getLogger('instance-discovery')
+    logger.setLevel(os.getenv('LOG_LEVEL', logging.INFO))
+
+    main(args)

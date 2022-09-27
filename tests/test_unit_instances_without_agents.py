ce574d0fd24a289497fb2c59e3aad728aa6e0d39
Michael Droessler <michael.droessler@lacework.net>
2022-09-27 07:05:58 -0500

    output stats

--- tests/test_unit_instances_without_agents.py
+++ tests/test_unit_instances_without_agents.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append('../instance-discovery')
 
 import instances_without_agents
+from argparse import Namespace
 
 #########################
 # check_truncation
@@ -56,14 +57,70 @@ def test_get_fargate_with_lacework_agents_1():
     assert(len(results_without_agent) == 1)
 
     
+###################################
+# apply_fargate_filter
+###################################
+def test_apply_fargate_filter_1():
+    # TODO: need to be able to mock LaceworkClinet.inventory.search
+    return 0
 
 
-# def test_normalization_agent_data_ec2():
+###################################
+# output_statistics
+###################################
+def test_output_statistics_current_account_1(capsys):
+    instances_without_agents_set = set()
+    instances_with_agents_set = set()
+    agents_without_inventory_set = set()
+    
+    input_instance_result = instances_without_agents.InstanceResult(instances_without_agents_set, instances_with_agents_set, agents_without_inventory_set)
+    input_user_profile_data = {'accounts':['test1']}
+    input_args = Namespace(account='', api_key='', api_secret='', subaccount='', profile='default', csv=False, json=True, debug=False, current_sub_account_only=True, statistics=False)
 
-# def test_normalization_agent_data_fargate():
+    instances_without_agents.output_statistics(input_args, input_instance_result, input_user_profile_data)
+    # capture output, k
+    out, err = capsys.readouterr()
+        
+    assert('Number of distinct hosts identified during inventory assessment: 0')
+    assert('Number of hosts which report successful agent operation: 0' in out)
+    assert('Coverage Percentage: 0%' in out)
+    assert(err == '')
 
-# def test_normalization_aws():
 
-# def test_normalization_gcp():
+def test_output_statistics_current_account_2(capsys):
+    instances_without_agents_set = set()
+    instances_with_agents_set = set([instances_without_agents.OutputRecord('test','',True,'test','test',{})])
+    agents_without_inventory_set = set()
+    
+    input_instance_result = instances_without_agents.InstanceResult(instances_without_agents_set, instances_with_agents_set, agents_without_inventory_set)
+    input_user_profile_data = {'accounts':['test1']}
+    input_args = Namespace(account='', api_key='', api_secret='', subaccount='', profile='default', csv=False, json=True, debug=False, current_sub_account_only=True, statistics=False)
 
-# def test_normalization_azure():
+    instances_without_agents.output_statistics(input_args, input_instance_result, input_user_profile_data)
+    # capture output, k
+    out, err = capsys.readouterr()
+        
+    assert('Number of distinct hosts identified during inventory assessment: 1')
+    assert('Number of hosts which report successful agent operation: 1' in out)
+    assert('Coverage Percentage: 100.0%' in out)
+    assert(err == '')
+    
+
+def test_output_statistics_current_account_3(capsys):
+    instances_without_agents_set = set([instances_without_agents.OutputRecord('test2','',True,'test','test',{})])
+    instances_with_agents_set = set([instances_without_agents.OutputRecord('test','',True,'test','test',{})])
+    agents_without_inventory_set = set()
+    
+    input_instance_result = instances_without_agents.InstanceResult(instances_without_agents_set, instances_with_agents_set, agents_without_inventory_set)
+    input_user_profile_data = {'accounts':['test1']}
+    input_args = Namespace(account='', api_key='', api_secret='', subaccount='', profile='default', csv=False, json=True, debug=False, current_sub_account_only=True, statistics=False)
+
+    instances_without_agents.output_statistics(input_args, input_instance_result, input_user_profile_data)
+    # capture output, k
+    out, err = capsys.readouterr()
+        
+    assert('Number of distinct hosts identified during inventory assessment: 2')
+    assert('Number of hosts which report successful agent operation: 1' in out)
+    assert('Number of hosts which report successful agent operation: 4' not in out)
+    assert('Coverage Percentage: 50.0%' in out)
+    assert(err == '')
